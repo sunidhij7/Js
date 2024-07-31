0c7e0dc124a9b03dcc6249a6d26b479cd93a8b5c
Sunidhi Jain <sunidhi.jain@brevo.com>
2024-07-31 13:55:29 +0530

    added basic accordian

--- script.js
+++ script.js
@@ -1,87 +1,103 @@
 let counter = 0;
 
-const counterValue = document.querySelector('.counter-value');
-const incrementBtn = document.querySelector('.increase');
-const decrementBtn = document.querySelector('.decrease');
-const resetBtn = document.querySelector('.reset');
+const counterValue = document.querySelector(".counter-value");
+const incrementBtn = document.querySelector(".increase");
+const decrementBtn = document.querySelector(".decrease");
+const resetBtn = document.querySelector(".reset");
 
 // To increment the value of counter
-incrementBtn.addEventListener('click', () => {
-    counter++;
-    counterValue.innerHTML = counter;
+incrementBtn.addEventListener("click", () => {
+  counter++;
+  counterValue.innerHTML = counter;
 });
 
 // To decrement the value of counter
-decrementBtn.addEventListener('click', () => {
-
-    if(counter>0){
-        counter--;
-    }
-    counterValue.innerHTML = counter;
+decrementBtn.addEventListener("click", () => {
+  if (counter > 0) {
+    counter--;
+  }
+  counterValue.innerHTML = counter;
 });
 
 // To reset the counter to zero
-resetBtn.addEventListener('click', reset);
+resetBtn.addEventListener("click", reset);
 
 function reset() {
-    counter = 0;
-    counterValue.innerHTML = counter;
+  counter = 0;
+  counterValue.innerHTML = counter;
 }
 
 // dropdown
 
-  const dropbtn = document.querySelector('.dropbtn');
-  const dropdown = document.querySelector('.dropdown-content');
-  const options = document.querySelectorAll('.dropdown-content div');
+const dropbtn = document.querySelector(".dropbtn");
+const dropdown = document.querySelector(".dropdown-content");
+const options = document.querySelectorAll(".dropdown-content div");
 
-  dropbtn.addEventListener('click', function() {
-    dropdown.classList.toggle('show');
+dropbtn.addEventListener("click", function () {
+  dropdown.classList.toggle("show");
+});
+
+// replacing drop button text
+options.forEach((option) => {
+  option.addEventListener("click", function (event) {
+    var selectedText = event.target.innerText;
+    dropbtn.innerText = selectedText;
+    dropdown.classList.remove("show");
   });
+});
 
-  // replacing drop button text
-  options.forEach(option => {
-    option.addEventListener('click', function(event) {
-      var selectedText = event.target.innerText;
-      dropbtn.innerText = selectedText;
-      dropdown.classList.remove('show');
-    });
-  });
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = dropdown;
+    if (dropdowns.classList.contains("show")) {
+      dropdowns.classList.remove("show");
+    }
+  } else if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 
-  
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = dropdown;
-      if (dropdowns.classList.contains('show')) {
-          dropdowns.classList.remove('show');
-        }
+// modal
+
+const openBtn = document.querySelector(".open-button");
+const modal = document.querySelector(".modal");
+const closeBtn = document.querySelector(".close-button");
+
+openBtn.onclick = function () {
+  modal.style.display = "block";
+};
+
+closeBtn.onclick = () => {
+  modal.style.display = "none";
+};
+
+//Closing windows
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = dropdown;
+    if (dropdowns.classList.contains("show")) {
+      dropdowns.classList.remove("show");
     }
   }
-
-  // modal
-
-  const openBtn = document.querySelector(".open-button");
-  const modal = document.querySelector(".modal");
-  const closeBtn = document.querySelector(".close-button");
-
-
-  openBtn.onclick = function() {
-    modal.style.display = "block";
-  }
-
-  closeBtn.onclick = () =>
-{
-  modal.style.display="none";
-}  
-
-window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+};
+
+// accordian
+
+const acc = document.querySelectorAll(".accordion");
+
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
 }
-
-
-
-
-
-  
-
