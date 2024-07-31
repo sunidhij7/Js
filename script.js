488ca56343af4249788e64814e7aaee1fde5714f
Sunidhi Jain <sunidhi.jain@brevo.com>
2024-07-31 11:32:18 +0530

    optimised js function

--- script.js
+++ script.js
@@ -36,15 +36,13 @@ function myFunction() {
   
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
+      var dropdowns = document.querySelector(".dropdown-content");
+      if (dropdowns.classList.contains('show')) {
+          dropdowns.classList.remove('show');
         }
-      }
     }
   }
+
+
   
 
